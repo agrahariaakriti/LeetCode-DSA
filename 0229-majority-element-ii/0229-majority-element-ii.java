67ea9d8ebdeacafb791a9d07fb5f690f84495b03
agrahariaakriti <114635032+agrahariaakriti@users.noreply.github.com>
2024-10-16 19:23:14 +0530

    Time: 5 ms (51.44%), Space: 48.4 MB (12.11%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> al=new HashMap<>();
-        
-        for(int i=0;i<nums.length;i++)
-        al.put(nums[i],al.getOrDefault(nums[i],0)+1);
-        ArrayList<Integer> al1=new ArrayList<>();
-        double n1=(nums.length)/3;
-        int n=(int)Math.floor(n1);
-        for(Map.Entry<Integer,Integer> e: al.entrySet())
+    ArrayList<Integer> al=new ArrayList<>();
+       Arrays.sort(nums);
+        if(nums.length==1)
         {
-            if(e.getValue()>n)
-            al1.add(e.getKey());
+            al.add(nums[0]);
+            return al;
         }
-        return al1;
+        if(nums.length==2)
+        {
+            if(nums[0]==nums[1])
+            {
+                al.add(nums[0]);
+                return al;
+            }
+            else
+            {
+                al.add(nums[0]);
+                al.add(nums[1]);
+                return al;
+            }
+        }
+
+
+
+        else 
+        {
+            double n1=(nums.length)/3;
+            int n=(int)Math.floor(n1);
+            int count=1;
+            for(int i=1;i<nums.length;i++)
+            {
+                if(nums[i-1]==nums[i])
+                {count++;
+                if(count>n && !al.contains(nums[i]))
+                al.add(nums[i]);
+                }
+                else{
+                count=1;}
+            }
+            return al;
+        }
+
 
 
     }
