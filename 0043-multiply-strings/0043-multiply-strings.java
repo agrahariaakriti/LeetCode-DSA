445f9b60ab9ad737bf4d8c36d859f995e638a582
agrahariaakriti <114635032+agrahariaakriti@users.noreply.github.com>
2024-10-28 21:43:21 +0530

    Time: 8 ms (28.46%), Space: 42.4 MB (50.26%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -0,0 +1,14 @@
+import java.math.*;
+class Solution {
+    public String multiply(String num1, String num2) { 
+    // return Integer.toString(Integer.parseInt(num1)*Integer.parseInt(num2));
+
+
+     BigInteger b1=new BigInteger(num1);
+     BigInteger b2=new BigInteger(num2); 
+        BigInteger b=b1.multiply(b2);
+    // long res=n1*n2;
+    return b.toString();
+    
+    }
+}
