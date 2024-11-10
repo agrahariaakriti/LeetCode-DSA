087b1e8fa3cd32d95657bf4ae028795b6c12c8e3
agrahariaakriti <114635032+agrahariaakriti@users.noreply.github.com>
2024-11-10 12:33:04 +0530

    Time: 1 ms (23.92%), Space: 41.3 MB (64.21%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,8 +1,53 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack.contains(needle))
-        return haystack.indexOf(needle);
-        else
-        return -1;
+        int lps[]=new int[needle.length()];
+        lps(needle,lps);
+       return kmp(haystack,needle,lps);
     }
+    void lps(String str,int lps[])
+    {
+        int len=0,i=1;
+        while(i<str.length())
+        {
+            if(str.charAt(i)==str.charAt(len))
+            {
+                len++;
+                lps[i]=len;i++;
+            }
+            else
+            {
+                if(len==0)
+                {
+                    lps[i]=0;
+                    i++;
+                }
+                else
+                len=lps[len-1];
+            }
+        }
+    }
+    int kmp(String txt ,String ptn , int lps[])
+    {
+        int n=txt.length();
+        int m=ptn.length();
+        int i=0,j=0;
+        while(i<n)
+        {
+            if(txt.charAt(i)==ptn.charAt(j))
+            {i++;j++;}
+            if(j==m)
+            {
+                return i-j;
+            }
+            else if(i<n && txt.charAt(i)!=ptn.charAt(j))
+            {
+                if(j==0)
+                i++;
+                else
+                j=lps[j-1];
+            }
+        }
+        return-1;
+    }
+
 }
