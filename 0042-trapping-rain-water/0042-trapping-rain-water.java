52d804d8e21c74350ba7335e247802cea1a843a4
agrahariaakriti <114635032+agrahariaakriti@users.noreply.github.com>
2024-12-15 08:08:03 +0530

    Time: 1 ms (65.25%), Space: 46.5 MB (25.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,23 +1,26 @@
 class Solution {
     public int trap(int[] height) {
+        int area=0;
+        int prefix[]= new int[height.length];
+        int suffix[]= new int[height.length];
         int n=height.length;
-     int lmax[]=new int[height.length];
-     int rmax[]=new int[height.length];
-     lmax[0]=height[0];
-     for(int i=1;i<height.length;i++)
-        lmax[i]=(int)Math.max(lmax[i-1],height[i]);
-     rmax[n-1]=height[n-1];
-     for(int i=n-2;i>=0;i--)
-        rmax[i]=(int)Math.max(rmax[i+1],height[i]);
-     int max=0,max_res=0;
-     for(int i=0;i<n;i++)
-     {
-        max=(int)Math.min(lmax[i],rmax[i]);
-        max_res+=max-height[i];
-     }
-     return max_res;
-
-
-
+        prefix[0]=height[0];
+        for(int i=1;i<n-1;i++)
+        {
+            prefix[i]=Math.max(prefix[i-1],height[i]);
+        }
+        suffix[n-1]=height[n-1];
+        for(int i=n-2;i>=1;i--)
+        {
+            suffix[i]=Math.max(suffix[i+1],height[i]);
+        }
+        for(int i=0;i<height.length-1;i++)
+        {
+            if(prefix[i]>height[i] && suffix[i]>height[i])
+            {
+                area+=(Math.min(suffix[i],prefix[i])-height[i]);
+            }
+        }
+        return area;
     }
 }
